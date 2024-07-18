5a14e047a1fe529ec7c0aff95507588cd7015055
Lev <vinnipyx38@gmail.com>
2024-07-18 15:35:58 +0500

    Added start messages handler

--- bot/message_handlers/__init__.py
+++ bot/message_handlers/__init__.py
@@ -1,7 +1,7 @@
-# from .start import StartHandler
+from .start import StartHandler
 # from .register import RegisterHandler
 # from .info import InfoHandler
 # from .order import OrderHandler, AcceptOrderHandler, DeclineOrderHandler
 # from .feedback import FeedbackHandler, FeedbackAnswerHandler
 
-LIST_HANDLERS = []
+LIST_HANDLERS = [StartHandler]
