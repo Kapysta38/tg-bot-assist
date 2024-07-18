bc46a5bf9a279545edf200997c52e7c94227ff30
Lev <vinnipyx38@gmail.com>
2024-07-18 16:28:40 +0500

    Upd logging handler

--- bot/config/logging_config.py
+++ bot/config/logging_config.py
@@ -1,25 +1,27 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import uuid
 
 
-def get_logger(filename='logs/app.log'):
+def get_logger(filename='logs/app.log', logger_name='app'):
     # Создание логгера
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger = logging.getLogger(logger_name)
 
-    # Формат логирования
-    formatter = logging.Formatter('[%(asctime)s | %(levelname)s | %(name)s]: %(message)s')
+    # Проверка, если логгер уже настроен
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
 
-    # Обработчик для логирования в консоль
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+        # Формат логирования
+        formatter = logging.Formatter('[%(asctime)s | %(levelname)s | %(name)s]: %(message)s')
 
-    # Обработчик для логирования в файл с ротацией
-    file_handler = RotatingFileHandler(filename, maxBytes=5 * 1024 * 1024, backupCount=5)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+        # Обработчик для логирования в консоль
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+        # Обработчик для логирования в файл с ротацией
+        file_handler = RotatingFileHandler(filename, maxBytes=5 * 1024 * 1024, backupCount=5)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     return logger
-
-
-
