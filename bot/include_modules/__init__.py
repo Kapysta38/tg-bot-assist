db668e34ffa45dd048ac23cf400477d790811879
Lev <vinnipyx38@gmail.com>
2024-07-18 17:21:42 +0500

    Added modules

--- bot/include_modules/__init__.py
+++ bot/include_modules/__init__.py
@@ -1,1 +1,10 @@
-LIST_MODULES = []
+LIST_MODULES = [(
+    "python E:/Programs/Programming/Code/freelance/GOOD_TG_BOT_LEHA/bot/include_modules/yandex-bot/parser.py",
+    'logs/ya-parser-bot.log'),
+    ("python E:/Programs/Programming/Code/freelance/GOOD_TG_BOT_LEHA/bot/include_modules/yandex-bot/yandex_bot.py",
+     'logs/yandex-bot.log'),
+    ("python E:/Programs/Programming/Code/freelance/GOOD_TG_BOT_LEHA/bot/include_modules/yandex-bot/auto_reg.py",
+     'logs/ya-autoreg-bot.log'),
+    ("python E:/Programs/Programming/Code/freelance/GOOD_TG_BOT_LEHA/bot/include_modules/yandex-bot/auto_reg_avto.py",
+     'logs/ya-autoreg-AVTO-bot.log')
+]
