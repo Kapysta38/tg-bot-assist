b5690d6ac628a99e2dc18dcc9a21b166772a1a7d
Lev <vinnipyx38@gmail.com>
2024-07-18 13:56:11 +0500

    Added logger generator

---
+++ bot/config/__init__.py
@@ -0,0 +1,1 @@
+from .logging_config import base_logger as log
---
+++ bot/config/logging_config.py
@@ -0,0 +1,25 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def get_logger(filename='app.log'):
+    # Создание логгера
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Формат логирования
+    formatter = logging.Formatter('[%(asctime)s | %(levelname)s | %(name)s]: %(message)s')
+
+    # Обработчик для логирования в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # Обработчик для логирования в файл с ротацией
+    file_handler = RotatingFileHandler(filename, maxBytes=5 * 1024 * 1024, backupCount=5)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+
+base_logger = get_logger()
